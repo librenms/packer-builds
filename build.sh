d0eeae2f3c2c91139f532c04be6afea1f6c73be9
CrazyMax <crazy-max@users.noreply.github.com>
2018-11-19 21:48:41 +0100

    Test

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 IMAGES=$(echo ${IMAGES:-"centos-7.5-x86_64;ubuntu-18.04-amd64"} | tr ';' '\n')
 BUILDERS=$(echo ${BUILDERS:-"virtualbox-iso"} | tr ';' '\n')
 RE='^[0-9]+([.][0-9]+)?$'
-LATEST_TAG=$(curl -s https://api.github.com/repos/librenms/librenms/releases/latest | jq -r ".tag_name")
+#LATEST_TAG=$(curl -s https://api.github.com/repos/librenms/librenms/releases/latest | jq -r ".tag_name")
+LATEST_TAG=1.45
 
 echo "Removing old files"
 rm -rf ./build/*
