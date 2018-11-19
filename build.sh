ae8752301dfa50715a0b86b6d69db7cbbb977496
CrazyMax <crazy-max@users.noreply.github.com>
2018-11-19 20:01:18 +0100

    Build and deploy through TravisCI

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+IMAGES=$(echo ${IMAGES:-"centos-7.5-x86_64;ubuntu-18.04-amd64"} | tr ';' '\n')
+BUILDERS=$(echo ${BUILDERS:-"virtualbox-iso"} | tr ';' '\n')
+RE='^[0-9]+([.][0-9]+)?$'
+#LATEST_TAG=1.45
+
+echo "Removing old files"
+rm -rf ./build/*
+
+if ! [[ "$LATEST_TAG" =~ $RE ]] ; then
+    echo "Tag not found"; exit 1;
+fi
+
+echo "Building images for $LATEST_TAG tag"
+for IMAGE in ${IMAGES}; do
+    for BUILDER in ${BUILDERS}; do
+        echo "Building $IMAGE with $BUILDER"
+        BUILD=$(PACKER_LOG=1 packer build -force -only=${BUILDER} -var 'librenms_version=$LATEST_TAG' -var 'headless=true' ${IMAGE}.json)
+        if [[ $? != 0 ]] ; then
+            echo "Build failed:"
+            echo ${BUILD}
+        else
+            echo "Build completed"
+        fi
+    done
+done
