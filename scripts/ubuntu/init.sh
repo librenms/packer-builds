d935e294435202b41bca8f2513a43d9ae78f2c7a
Andrew Piroli <45859969+AndrewPiroli@users.noreply.github.com>
2021-02-18 00:59:47 -0500

    Ubuntu 20.04 Update done for real this time

--- scripts/ubuntu/init.sh
+++ scripts/ubuntu/init.sh
@@ -2,7 +2,7 @@
 
 set -e
 set -x
-
+sudo lvextend -l +100%FREE /dev/mapper/ubuntu--vg-ubuntu--lv --resizefs
 sudo sed -i -e 's,^\(ACTIVE_CONSOLES="/dev/tty\).*,\11",' /etc/default/console-setup
 for f in /etc/init/tty[^1]*.conf; do
   sudo mv "$f"{,.bak}
